9f3c8d6d03f46983173b994afa92f996da993dba
Kauany lima <kauany.lima@estudante.ifro.edu.br>
2023-06-20 18:42:48 -0400

    Listboxpo2

---
+++ listboxpo2/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace listboxpo2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.lb_Animais = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button1.Location = new System.Drawing.Point(27, 292);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 33);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Adicionar";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(28, 341);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(119, 30);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "remover";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(27, 390);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(120, 35);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "apagar";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // lb_Animais
+            // 
+            this.lb_Animais.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_Animais.ForeColor = System.Drawing.Color.Black;
+            this.lb_Animais.FormattingEnabled = true;
+            this.lb_Animais.ItemHeight = 20;
+            this.lb_Animais.Location = new System.Drawing.Point(27, 82);
+            this.lb_Animais.Name = "lb_Animais";
+            this.lb_Animais.Size = new System.Drawing.Size(120, 164);
+            this.lb_Animais.TabIndex = 3;
+            this.lb_Animais.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(27, 37);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(152, 22);
+            this.textBox1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Yellow;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lb_Animais);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListBox lb_Animais;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ listboxpo2/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace listboxpo2
+{
+    public partial class Form1 : Form
+    {
+        List<string> animais = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+            animais.Add("zebra");
+            animais.Add("camelo");
+            animais.Add("arara");
+            lb_Animais.DataSource = animais;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            animais.Clear();
+            lb_Animais.DataSource = null;
+            lb_Animais.DataSource = animais;
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                MessageBox.Show("Digite um animal!");
+            }
+
+            else
+            {
+                animais.Add(textBox1.Text);
+                textBox1.Clear();
+                lb_Animais.DataSource = null;
+                lb_Animais.DataSource = animais;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Exitem dois métodos, são eles:
+
+            /*1º método
+             remove o item da posição selecionada
+            */
+            animais.RemoveAt(lb_Animais.SelectedIndex);
+            lb_Animais.DataSource = null;
+           lb_Animais.DataSource = animais;
+            
+
+            /* 2° método 
+            remove a partir do texto escrito
+            
+            animais.Remove(textBox1.Text);
+            textBox1.Clear();
+            lb_Animais.DataSource = null;
+            lb_Animais.DataSource = animais;
+            */ 
+            
+        }
+    }
+}
